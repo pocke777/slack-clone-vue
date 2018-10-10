1b951c1120325265d09b6e67deff5b50e9936c71
川崎暑士 <kawasaki8910@gmail.com>
2018-10-10 15:34:38 +0900

    モックデータに反映

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,3 +1,27 @@
+const messages = [
+  {
+    name: 'scottish-fold',
+    time: '11:10 AM',
+    icon: 'images/pickrusu.jpg',
+    text: 'これはサンプルテキストですこれはサンプルテキストです\nこれはサンプルテキストです\nこれはサンプルテキストですこれはサンプルテキストですこれはサンプルテキストです',
+  },
+  {
+    name: 'scottish-fold',
+    time: '11:10 AM',
+    icon: 'images/pickrusu.jpg',
+    text: 'これはサンプルテキストですこれはサンプルテキストです\nこれはサンプルテキストです\nこれはサンプルテキストですこれはサンプルテキストですこれはサンプルテキストです',
+  },
+  {
+    name: 'scottish-fold',
+    time: '11:10 AM',
+    icon: 'images/pickrusu.jpg',
+    text: 'これはサンプルテキストですこれはサンプルテキストです\nこれはサンプルテキストです\nこれはサンプルテキストですこれはサンプルテキストですこれはサンプルテキストです',
+  }
+]
+
 new Vue({
-  el: "#app"
+  el: "#app",
+  data: {
+    messages: messages,
+  }
 })
